a8e329ffc7355e837805e52ec629e39920ad4582
lenny.thieuleux <lennylen4@gmail.com>
2020-03-01 19:03:40 +0100

    modify admin-setup link

--- dashboard.php
+++ dashboard.php
@@ -31,7 +31,7 @@
             <div class="admin-card">
                 <h2>Espace administrateur</h2>
                 <ul>
-                    <li><a href="admin-setup.php">Créer un nouveau compte administrateur</a></li>
+                    <li><a href="admin-setup.html">Créer un nouveau compte administrateur</a></li>
                     <li>Gérer les comptes administrateurs existants</li>
                 </ul>
             </div>
